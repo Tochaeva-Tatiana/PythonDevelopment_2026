2b262dafcfbd0ec06d57db3627e90221b7f2a3c3
Tatiana Tochaeva <t.tochaeva@yandex.ru>
2026-02-24 23:22:41 +0300

    Cows in one line

--- 02_PushPip/twocows.py
+++ 02_PushPip/twocows.py
@@ -17,6 +17,19 @@ args = parser.parse_args()
 cow1 = cowsay(args.message_1, eyes = args.e, cow=args.f, wrap_text=not(args.n))
 cow2 = cowsay(args.message_2, eyes = args.E, cow=args.F, wrap_text=not(args.N))
 
+cow1_split = cow1.split('\n')
+cow2_split = cow2.split('\n')
 
-print(cow1)
-print(cow2)
+max_lenght_1 = max([len(i) for i in cow1_split])
+
+max_weight_1 = len(cow1_split)
+max_weight_2 = len(cow2_split)
+
+if max_weight_1 > max_weight_2:
+    cow2_split = [" "] * (max_weight_1 - max_weight_2) + cow2_split
+else:
+    cow1_split = [" "] * (max_weight_2 - max_weight_1) + cow1_split
+
+for i in range(max(max_weight_1, max_weight_2)):
+    len_line = len(cow1_split[i])
+    print(cow1_split[i] + ' ' * (max_lenght_1 - len_line), cow2_split[i])
